95b9dfd4b75fcbfb10106595e047d054b849b82e
bruno.mollo <bruno.mollo@syloper.com>
2025-12-22 15:11:39 -0300

    install express

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (_, res) => {
+  res.send("API de pan dulces");
+});
+
+app.listen(port, () => {
+  console.log(`Pan dulces API corriendo en puerto ${port}`);
+});
